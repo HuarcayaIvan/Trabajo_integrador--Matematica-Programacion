5a3d9c968acf6158beaa251fabbfe13ef007f192
Huarcaya Ivan <hf.rivan@gmail.com>
2025-04-16 21:29:56 -0300

    Condicionales por frecuencia ALTA MEDIA y BAJA terminadas

--- Trabajo Integrador.py
+++ Trabajo Integrador.py
@@ -25,32 +25,108 @@ else:
     Frecuencia = input("Ingrese la frecuencia de uso segun las siguientes categorias [ALTA] [MEDIA] [BAJA].").upper()
     Actividad = input("Ingrese el tipo de actividad [RURAL] [URBANA] [COMERCIAL].").upper()
 
-#///// Inicio de estructura condicional para la recomendacion de aseguradora frecuencia ALTA /////
+#///// Inicio de estructura condicional para la recomendacion de aseguradora /////
+
+#------------------- FRECUENCIA ALTA -------------------
 if ((Frecuencia == "ALTA") and (Actividad == "URBANA")):
     if 45000 <= Presupuesto < 75000 :
         print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN1}. ")
     elif 75000 <= Presupuesto < 120000 :
         print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}. ")
-    else:
-        Presupuesto >= 120000 
+    elif Presupuesto >= 120000 :
         print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN3}. ")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
 
-if (Frecuencia == "ALTA") and (Actividad == "RURAL") and (Presupuesto > 90000):
+
+if (Frecuencia == "ALTA") and (Actividad == "RURAL"):
     if 90000 <= Presupuesto < 150000 :
         print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN1}. ")
     elif 150000 <= Presupuesto < 200000 :
         print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}. ")
+    elif Presupuesto >= 200000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN3}. ")
     else:
-        Presupuesto >= 200000
-        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN3}. ")
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
 
-if (Frecuencia == "ALTA") and (Actividad == "COMERCIAL") and (Presupuesto > 60000):
-    if 60000 <= Presupuesto < 85000 :
-        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN1}. ")
-    elif 85000 <= Presupuesto < 150000 :
-        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}. ")
-    else :
-        Presupuesto >= 150000
-        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN3}. ")
-else:
-    print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+if ((Frecuencia == "ALTA") and (Actividad == "COMERCIAL")):
+    if 60000 <= Presupuesto < 85000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN1}.")
+    elif 85000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}.")
+    elif Presupuesto >= 150000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN3}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+
+#------------------- FRECUENCIA MEDIA -------------------
+if ((Frecuencia == "MEDIA") and (Actividad == "URBANA")):
+    if 75000 <= Presupuesto < 120000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}.")
+    elif 150000 <= Presupuesto < 200000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN2}.")
+    elif Presupuesto >= 200000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN2}.")
+    elif 85000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN2}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+
+if ((Frecuencia == "MEDIA") and (Actividad == "RURAL")):
+    if 120000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN3}.")
+    elif 150000 <= Presupuesto < 200000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN3}.")
+    elif Presupuesto >= 200000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN3}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+
+if ((Frecuencia == "MEDIA") and (Actividad == "COMERCIAL")):
+    if 75000 <= Presupuesto < 120000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}.")
+    elif 150000 <= Presupuesto < 200000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN2}.")
+    elif 85000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN2}.")
+    elif Presupuesto >= 200000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN2}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+
+#------------------- FRECUENCIA BAJA -------------------
+if ((Frecuencia == "BAJA") and (Actividad == "URBANA")):
+    if 45000 <= Presupuesto < 75000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN1}.")
+    elif 90000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN1}.")
+    elif 60000 <= Presupuesto < 85000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN1}.")
+    elif Presupuesto >= 150000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN1}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+
+if ((Frecuencia == "BAJA") and (Actividad == "RURAL")):
+    if 75000 <= Presupuesto < 120000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN2}.")
+    elif 150000 <= Presupuesto < 200000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN2}.")
+    elif 85000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN2}.")
+    elif Presupuesto >= 200000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN2}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
+
+if ((Frecuencia == "BAJA") and (Actividad == "COMERCIAL")):
+    if 45000 <= Presupuesto < 75000:
+        print(f"Le recomendamos contratar {SEGURO1} con el plan {PLAN1}.")
+    elif 90000 <= Presupuesto < 150000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN1}.")
+    elif 60000 <= Presupuesto < 85000:
+        print(f"Le recomendamos contratar {SEGURO2} con el plan {PLAN1}.")
+    elif Presupuesto >= 150000:
+        print(f"Le recomendamos contratar {SEGURO3} con el plan {PLAN1}.")
+    else:
+        print("Lo sentimos, no hay planes disponibles para su presupuesto.")
